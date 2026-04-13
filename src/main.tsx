c901a5e8b7bc911ba78d81d8f1d8e56767943e7d
zhaohernando-code <zhaohernando@gmail.com>
2026-04-13 14:17:56 +0800

    chore(sync): apply task-mnwslvvq-03y8jh -> src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "antd/dist/reset.css";
 import App from "./App";
 import "./styles.css";
 
@@ -8,4 +9,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <App />
   </React.StrictMode>,
 );
-
